2e64e889f715d574e3e859d2e3dde1c6d2b76348
Victor Borshchov <victor.borshchov@gmail.com>
2016-10-20 20:24:19 +0300

    Adds program with triangle square

---
+++ triangle_square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("Input 2 number(sides of right triangle) to get square of triangle\n");
+
+first_side = float(input("first side: "))
+second_side = float(input("second side: "))
+square = first_side * second_side / 2
+
+print("Square of triangle is %s" % square);
