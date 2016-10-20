573787dc4f5979fe49e8802e5930005b931ca476
Victor Borshchov <victor.borshchov@gmail.com>
2016-10-20 20:16:33 +0300

    Add program with sum of three number

---
+++ sum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("Input 3 number to get sum of them\n");
+
+first = int(input("first number: "))
+second = int(input("second number: "))
+third = int(input("third number: "))
+sum = first + second + third
+
+print("Sum of three number is %s" % sum);
